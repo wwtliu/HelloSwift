6245d84ebe9485261d5a09a866127f3dc27d2136
wwtliu <liuxingyu>
2014-06-22 15:43:47 +0800

    swift
    swift test

---
+++ L01HelloSwift/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  L01HelloSwift
+//
+//  Created by wwtliu on 14-6-12.
+//  Copyright (c) 2014年 wwtliu. All rights reserved.
+//
+
+import Foundation
+
+var a = 1
+a = 10
+let c = a*a
+println(c)
